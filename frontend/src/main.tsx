111719da30fc82f3a1aba80cdf52ea74a99f87e9
Nicolette Mashaba <nicolette.mashaba@techbridlefoundation.org>
2026-02-14 00:40:53 +0200

    Restore lightweight preloader with safe auto-dismiss

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const loader = document.getElementById("app-loader");
+
 createRoot(document.getElementById("root")!).render(<App />);
+
+if (loader) {
+  window.setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.pointerEvents = "none";
+
+    window.setTimeout(() => {
+      loader.remove();
+    }, 350);
+  }, 400);
+}
