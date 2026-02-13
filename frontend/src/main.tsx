5faca921dfab915cfddfba838342d44061efb198
Nicolette Mashaba <nicolette.mashaba@techbridlefoundation.org>
2026-02-14 00:37:53 +0200

    Stabilize frontend boot and enforce SPA routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,21 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const loader = document.getElementById("app-loader");
-const bootFlag = "__CALM_APP_BOOTED__";
-
 createRoot(document.getElementById("root")!).render(<App />);
-
-window[bootFlag as keyof Window & string] = true;
-
-if (loader) {
-  window.setTimeout(() => {
-    loader.style.transition = "opacity 0.35s ease-out";
-    loader.style.opacity = "0";
-    loader.style.pointerEvents = "none";
-
-    window.setTimeout(() => {
-      loader.remove();
-    }, 350);
-  }, 450);
-}
