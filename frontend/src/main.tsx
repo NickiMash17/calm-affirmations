40eb758ec9b6618b0e13122237c3a735805b0923
Nicolette Mashaba <nicolette.mashaba@techbridlefoundation.org>
2026-02-13 18:46:55 +0200

    Update favicon and add app loader

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const loader = document.getElementById("app-loader");
+
 createRoot(document.getElementById("root")!).render(<App />);
+
+if (loader) {
+  loader.style.opacity = "0";
+  loader.style.pointerEvents = "none";
+  window.setTimeout(() => loader.remove(), 300);
+}
